51676e7592cb47ca5a3f6e2e60c9d6eaa9734401
Yi-Cheng Chen <Chad.CYB@gmail.com>
2015-07-28 13:26:41 +0800

    7/28

--- apply.php
+++ apply.php
@@ -3,17 +3,30 @@
 include 'php/mysql_conn.php';
 include 'fun.inc.php';
 $row = @mysql_query ( "SELECT * FROM `info` WHERE `ID` = '" . $_SESSION ['userID'] . "' " );
-// print_r($row);
-// print_r($_SESSION['userID']);
-// !@mysql_fetch_array($row) !isset($_SESSION['userID']
 if (! @mysql_fetch_array ( $row )) {
 	echo "<script>
 				alert('請先登入');
 				window.location = 'weblogin.php';
-			  </script>";
+		</script>";
 	exit ();
 }
-$rsl = mysql_query ( "SELECT * FROM river WHERE 1" );
+/* SELECT
+	competition.matchID,
+	river.Name,
+	competition.difficulty,
+	competition.price,
+	competition.maxPeople
+   FROM
+	competition,
+	river,
+	game
+   WHERE
+	competition.matchID = game.matchID
+	AND river.No = competition.riverID
+ */
+$rsl = mysql_query ( "SELECT competition.matchID, river.Name, competition.difficulty, competition.price,  
+		competition.maxPeople FROM competition, river, game WHERE competition.matchID = game.matchID 
+		AND river.No = competition.riverID AND competition.status = 0" );
 $num = mysql_num_rows ( $rsl );
 ?>
 <html>
@@ -22,84 +35,77 @@ $num = mysql_num_rows ( $rsl );
 <meta charset="UTF-8">
 <meta name="robots" content="noindex">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet"
-	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-<link rel="stylesheet"
-	href="http://www.justinaguilar.com/animations/css/animations.css">
-<script src="http://code.jquery.com/jquery-latest.min.js"
-	type="text/javascript"></script>
-<script
-	src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<link rel="stylesheet" href="http://www.justinaguilar.com/animations/css/animations.css">
+<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+<script	src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 </head>
-<body style="background-image: url('images/apply-bg.jpg')">
+<body style="background-image: url('images/apply-bg.jpg'); height:100%">
 		<?php topNavBarLogin(102);?>
-		<div class="container" style="margin-top: 50px;">
+		<div class="container" style="margin-top: 50px">
 		<h1 style="font-weight: bold">線上泛舟報名</h1>
-		<table class="table table-hover"
-			style="font-size: 18px; font-weight: bold">
+		<table class="table table-hover" style="font-size: 18px; font-weight: bold">
 			<thead>
 				<tr>
 					<th>泛舟梯次</th>
 					<th>泛舟河流</th>
 					<th>泛舟難度</th>
+					<th>報名費用</th>
 					<th>泛舟人數</th>
 					<th>報名人數</th>
+					<th>泛舟日期</th>
 					<th></th>
 				</tr>
 			</thead>
 			<tbody>
 				<?php
-				$rnd = rand ( 1, 12 );
-				for($i = 0; $i < $rnd; $i ++) {
-					$rows = mysql_fetch_array ( $rsl );
-					$no = $i + 1;
-					$rnd1 = rand ( 0, 200 );
-					$rnd2 = rand ( 0, $rnd1 );
+				for($i = 0; $i < $num ; $i ++) {
+					$rows = @mysql_fetch_array ( $rsl );
+					$rsCount = @mysql_query ( "SELECT COUNT(*) FROM `game` WHERE game.matchID = '" . $rows ['matchID'] . "' " );
+					$count = @mysql_fetch_row ( $rsCount ); // <<<報名人數
 					echo '<tr>
-							<th scope="row">' . $no . '</th>
-							<td>' . $rows [1] . '</td>
-							<td>LV.' . rand ( 1, 50 ) . '</td>
-							<td>' . $rnd1 . '</td>
-							<td>' . $rnd2 . '</td>';
-					if ($rnd1 == $rnd2) {
-						echo '<td><button type="button" class="btn btn-danger" disabled="disabled"">額滿截止</button></td>	</tr>';
+							<th scope="row">' . $rows ['matchID'] . '</th>
+							<td>' . $rows ['Name'] . '</td>
+							<td>LV.' . $rows ['difficulty'] . '</td>
+							<td>' . $rows ['price'] . '萬</td>
+							<td>' . $rows ['maxPeople'] . '</td>
+							<td>' . $count [0] . '</td>';
+					if ($rows ['maxPeople'] > $count [0]) {
+						echo '<td><button type="button" class="btn btn-success" data-toggle="modal" data-target="#myModal"
+								onclick="catchClick(' . $rows [1] . ')">前往報名</button></td></tr>';
 					} else {
-						echo '<td><button type="button" class="btn btn-success">前往報名</button></td>	</tr>';
+						echo '<td><button type="button" class="btn btn-danger" disabled="disabled"">額滿截止</button></td></tr>';
 					}
 				}
 				?>
-				<!-- 
-				<tr>
-					<th scope="row">1</th>
-					<td>濁水溪</td>
-					<td>LV 5</td>
-					<td>120</td>
-					<td>25</td>
-					<th><button type="button" class="btn btn-success btn-lg">前往報名</button></th>
-				</tr>
-				<tr>
-					<th scope="row">2</th>
-					<td>秀姑巒溪</td>
-					<td>LV 8</td>
-					<td>80</td>
-					<td>5</td>
-					<th><button type="button" class="btn btn-success btn-lg">前往報名</button></th>
-				</tr>
-				<tr>
-					<th scope="row">3</th>
-					<td>淡水河</td>
-					<td>LV 1</td>
-					<td>200</td>
-					<td>200</td>
-					<th><button type="button" class="btn btn-danger btn-lg" disabled="disabled"">額滿截止</button></th>
-				</tr>
-					 -->
 			</tbody>
 		</table>
 		<form action="apply.php" method="post">
 			<Input type="button" class="btn btn-primary btn-lg" value="登出"
 				onclick="location.href='logout.php'">
 		</form>
+	</div>
+	<div class="modal fade" id="myModal" tabindex="-1" role="dialog"
+		aria-labelledby="myModalLabel">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal"
+						aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+					<h4 class="modal-title" id="myModalLabel">梯次</h4>
+				</div>
+				<div class="modal-body">
+					<p>自動排水舟</p>
+					<p>動力舟</p>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					<button type="button" class="btn btn-primary">Save changes</button>
+				</div>
+			</div>
+		</div>
 	</div>
 </body>
 </html>
--- user.php
+++ user.php
@@ -4,64 +4,74 @@ include 'php/mysql_conn.php';
 include 'fun.inc.php';
 $row = @mysql_query ( "SELECT * FROM `info` WHERE `ID` = '" . $_SESSION ['userID'] . "' " );
 if (! $result = @mysql_fetch_array ( $row )) {
-	echo "<script>
-				alert('請先登入');
-				window.location = 'weblogin.php';
-			  </script>";
+	echo "<script> alert('請先登入'); window.location = 'weblogin.php'; </script>";
 	exit ();
+} else {
+	/*
+	 * SELECT
+	 * game.matchID, river.Name,
+	 * competition.difficulty, competition.price,
+	 * competition.matchDate, game.status
+	 * FROM
+	 * game, river, competition ,info
+	 * WHERE
+	 * game.matchID = competition.matchID AND competition.riverID = river.No
+	 * AND game.userID = info.userID AND info.ID = 103001
+	 */
+	$rowGame = @mysql_query ( "SELECT game.matchID, river.Name, competition.difficulty, competition.price, competition.matchDate, game.status FROM game, river, competition ,info WHERE game.matchID = competition.matchID AND competition.riverID = river.No AND game.userID = info.userID AND info.ID = '" . $_SESSION ['userID'] . "' " );
+	$numGame = mysql_num_rows ( $rowGame );
 }
 ?>
 <html>
 <head>
-<title>亞洲泛舟網</title>
-<meta charset="UTF-8">
-<meta name="robots" content="noindex">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet"
-	href="http://www.justinaguilar.com/animations/css/animations.css">
-<link rel="stylesheet"
-	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-<link rel="stylesheet"
-	href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-<script src="http://code.jquery.com/jquery-latest.min.js"
-	type="text/javascript"></script>
-<script
-	src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-<?php
-function get_exp($exp) {
-	$lv = 1;
-	$lvExp = 100;
-	while ( $exp >= $lvExp ) {
-		$lv ++;
-		$exp -= $lvExp;
-		$lvExp = floor ( $lvExp * 1.2 );
-	}
-	return $exp;
-}
-function get_LvExp($exp) {
-	$lv = 1;
-	$lvExp = 100;
-	while ( $exp >= $lvExp ) {
-		$lv ++;
-		$exp -= $lvExp;
-		$lvExp = floor ( $lvExp * 1.2 );
-	}
-	return $lvExp;
-}
-function get_LvPa($ex1, $ex2) {
-	return floor ( ($ex1 / $ex2) * 100 );
-}
-function get_Lv($exp) {
-	$lv = 1;
-	$lvExp = 100;
-	while ( $exp >= $lvExp ) {
-		$lv ++;
-		$exp -= $lvExp;
-		$lvExp = floor ( $lvExp * 1.2 );
-	}
-	return $lv;
-}
-?>
+	<title>亞洲泛舟網</title>
+	<meta charset="UTF-8">
+	<meta name="robots" content="noindex">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="http://www.justinaguilar.com/animations/css/animations.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+	<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+	<script	src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	<?php
+		function get_exp($exp) {
+			$lv = 1;
+			$lvExp = 100;
+			while ( $exp >= $lvExp ) {
+				$lv ++;
+				$exp -= $lvExp;
+				$lvExp = floor ( $lvExp * 1.2 );
+			}
+			return $exp;
+		}
+		function get_LvExp($exp) {
+			$lv = 1;
+			$lvExp = 100;
+			while ( $exp >= $lvExp ) {
+				$lv ++;
+				$exp -= $lvExp;
+				$lvExp = floor ( $lvExp * 1.2 );
+			}
+			return $lvExp;
+		}
+		function get_Lv($exp) {
+			$lv = 1;
+			$lvExp = 100;
+			while ( $exp >= $lvExp ) {
+				$lv ++;
+				$exp -= $lvExp;
+				$lvExp = floor ( $lvExp * 1.2 );
+			}
+			return $lv;
+		}
+		// --------------------------------------------------
+		function get_LvPa($ex1, $ex2) {
+			return floor ( ($ex1 / $ex2) * 100 );
+		}
+		function get_LvMoney($LV) {
+			return (100 + 10 * ($LV - 1));
+		}
+	?>
 </head>
 <body style="background-image: url('images/apply-bg.jpg')">
 		<?php topNavBarLogin(101);?>
@@ -84,9 +94,9 @@ function get_Lv($exp) {
 					<h4>經驗值 (<?php echo get_exp($result['Exp']);?>/<?php echo get_lvExp($result['Exp']);?>)</h4>
 					<div class="progress">
 						<div class="progress-bar" role="progressbar" aria-valuenow="0"
-									aria-valuemin="0" aria-valuemax="100"
-									style="width: <?php echo get_LvPa(get_exp($result['Exp']),get_lvExp($result['Exp']));?>%;">
-									<?php echo get_LvPa(get_exp($result['Exp']),get_lvExp($result['Exp']));?>%
+							aria-valuemin="0" aria-valuemax="100"
+							style="width: <?php echo get_LvPa(get_exp($result['Exp']),get_lvExp($result['Exp']));?>%;">
+							<?php echo get_LvPa(get_exp($result['Exp']),get_lvExp($result['Exp']));?>%
 						</div>
 					</div>
 				</div>
@@ -94,19 +104,18 @@ function get_Lv($exp) {
 		</div>
 		<div class="row">
 			<div class="col-md-3">
-				<button class="btn btn-success fa fa-money" type="button"
-					style="font-size: 40px">
-					金庫等級 <span class="badge" style="font-size: 40px"><?php echo '1';?></span>
+				<button class="btn btn-success fa fa-money" type="button" style="font-size: 40px">
+					金庫等級 <span class="badge" style="font-size: 40px"><?php echo $result['bankLV'];?></span>
 				</button>
 			</div>
 			<div class="col-md-8 col-md-offset-1">
 				<div class="alert alert-warning" role="alert">
-					<h4>餘額 (<?php echo '50/100'; ?>)萬</h4>
+					<h4>餘額 (<?php echo $result['Money'].'/'.get_LvMoney($result['bankLV']); ?>)萬</h4>
 					<div class="progress">
 						<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="0"
-									aria-valuemin="0" aria-valuemax="100"
-									style="width: <?php echo '50'; ?>%;">
-									<?php echo '50'; ?>%
+							aria-valuemin="0" aria-valuemax="100"
+							style="width: <?php echo get_LvPa($result['Money'],get_LvMoney($result['bankLV'])); ?>%;">
+							<?php echo get_LvPa($result['Money'],get_LvMoney($result['bankLV'])); ?>%
 						</div>
 					</div>
 				</div>
@@ -114,8 +123,7 @@ function get_Lv($exp) {
 		</div>
 		<div class="alert alert-info" role="alert">
 			<h3 style="text-align: center; font-weight: bold;">哥的泛舟史</h3>
-			<table class="table table-hover"
-				style="font-size: 18px; color: black">
+			<table class="table table-hover" style="font-size: 18px; color: black">
 				<thead>
 					<tr>
 						<th>泛舟梯次</th>
@@ -130,16 +138,14 @@ function get_Lv($exp) {
 				<tbody>
 				<?php
 				$rnd = rand ( 1, 12 );
-				for($i = 0; $i < 150; $i ++) {
-					$no = $i + 1;
-					$rnd1 = rand ( 0, 200 );
-					$rnd2=	rand ( 0, 100 );
-					$money1=	rand ( 0, 100 );
+				for($i = 0; $i < $numGame; $i ++) {
+					$rows = mysql_fetch_array ( $rowGame );
+					$rnd2 = rand ( 0, 100 );
 					echo '<tr>
-							<th scope="row">' . $no . '</th>
-							<td>LV.' . rand ( 1, 50 ) . '</td>
-							<td>' . $rnd1 . '</td>
-			  				<td>' . $money1 . '萬</td>
+							<th scope="row">' . $rows ['matchID'] . '</th>
+							<td>' . $rows ['Name'] . '</td>
+							<td>LV.' . $rows ['difficulty'] . '</td>
+							<td>' . $rows ['price'] . '萬</td>
 							<td><div class="progress">
 								<div class="progress-bar" role="progressbar" aria-valuenow="0"
 									aria-valuemin="0" aria-valuemax="100"
@@ -149,14 +155,13 @@ function get_Lv($exp) {
 							</div></td>';
 					if (100 == $rnd2) {
 						echo '<td><button type="button" class="btn btn-success">泛舟完成</button></td>	</tr>';
-					} else if(0 == $rnd2) {
+					} else if (0 == $rnd2) {
 						echo '<td><button type="button" class="btn btn-danger" disabled="disabled">
 								尚未開始...</button></td>	</tr>';
 					} else {
 						echo '<td><button type="button" class="btn btn-warning" disabled="disabled">
 								泛舟中...</button></td>	</tr>';
 					}
-					
 				}
 				?>
 			</tbody>
