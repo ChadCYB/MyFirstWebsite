f59ee7409027da40c8427c688873e1d79a5d49cf
Yi-Cheng Chen <Chad.CYB@gmail.com>
2015-07-30 13:49:19 +0800

    7/30
    新增報名功能

--- Rafting/pass.apply.php
+++ Rafting/pass.apply.php
@@ -24,7 +24,7 @@ if (! @mysql_fetch_array ( $row )) {
 	river.No = competition.riverID
 	AND competition.status = 0
  */
-$rsl = mysql_query ( "SELECT competition.matchID, river.Name, competition.difficulty, competition.price,  
+$rsl = @mysql_query ( "SELECT competition.matchID, river.Name, competition.difficulty, competition.price,  
 		competition.maxPeople, competition.matchDate FROM competition, river WHERE river.No = competition.riverID
 		AND competition.status = 0" );
 $num = mysql_num_rows ( $rsl );
@@ -57,6 +57,8 @@ $num = mysql_num_rows ( $rsl );
 					$rows = @mysql_fetch_array ( $rsl );
 					$rsCount = @mysql_query ( "SELECT COUNT(*) FROM `game` WHERE game.matchID = '" . $rows ['matchID'] . "' " );
 					$count = @mysql_fetch_row ( $rsCount ); // <<<報名人數
+					$sqlCheck = @mysql_query("SELECT * FROM game WHERE matchID='" . $rows['matchID']. "' AND userID='" . $_SESSION['uID']. "'");
+					$numCheck = mysql_num_rows ( $sqlCheck );	// <<<有沒有報名過
 					echo '<tr>
 							<th scope="row">' . $rows['matchID'] . '</th>
 							<td>' . $rows ['Name'] . '</td>
@@ -66,17 +68,24 @@ $num = mysql_num_rows ( $rsl );
 							<td>' . $count [0] . '</td>
 							<td>' . $rows ['matchDate'] . '</td>';
 					if ($rows ['maxPeople'] > $count [0]) {
-						echo '<td><input type="submit" class="btn btn-success" data-toggle="modal" 
+						if($numCheck != 0){
+							echo '<td><button type="button" class="btn btn-danger" 
+								disabled="disabled">已報名</td></tr>';
+						}else{
+							echo '<td><input type="submit" class="btn btn-success" data-toggle="modal"
 								data-target="#myModal'.$rows['matchID'].'" value="前往報名"></td></tr>';
+						}
 					} else {
 						echo '<td><button type="button" class="btn btn-danger" disabled="disabled">
 								額滿截止</button></td></tr>';
 					}
-				echo'
-					<div class="modal fade" id="myModal'.$rows['matchID'].'" tabindex="-1"
-						role="dialog" aria-labelledby="myModalLabel">
-					<div class="modal-dialog" role="document">
-					<div class="modal-content">
+					if($numCheck == 0){
+						echo'
+						<form action="pass.apply.php" method="post">
+						<div class="modal fade" id="myModal'.$rows['matchID'].'" tabindex="-1"
+							role="dialog" aria-labelledby="myModalLabel">
+						<div class="modal-dialog" role="document">
+						<div class="modal-content">
 						<div class="modal-header">
 							<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 								<span aria-hidden="true">&times;</span>
@@ -88,76 +97,59 @@ $num = mysql_num_rows ( $rsl );
 						<div class="modal-body bg-info">
 							<div class="form-group has-success">
 							  <div class="input-group">
-							    <span class="input-group-addon"></span>
+							    <span class="input-group-addon">等級限制</span>
 								<input class="form-control input-lg" style="font-weight:bold"
-								type="text" value="等級限制' . $rows ['difficulty'] . '等" readonly>
+								type="text" value="'. $rows ['difficulty'] .'" readonly>
+								<span class="input-group-addon">等</span>
 							  </div>
 							</div>
 							<div class="form-group has-success">
 							  <div class="input-group">
-							    <span class="input-group-addon"></span>
+							    <span class="input-group-addon">報名費用</span>
 								<input class="form-control input-lg" style="font-weight:bold"
-								type="text" value="報名費用' . $rows ['price'] . '萬" readonly>
+								type="text" value="'.$rows ['price'].'" readonly>
+								<span class="input-group-addon">萬</span>
 							  </div>
 							</div>
 							<div class="form-group has-success">
 							  <div class="input-group">
-							    <span class="input-group-addon"></span>
+							    <span class="input-group-addon">泛舟日期</span>
 								<input class="form-control input-lg" style="font-weight:bold"
-								type="text" value="泛舟日' . $rows ['matchDate'] . '" readonly>
+								type="text" value="'. $rows ['matchDate'] .'" readonly>
 							  </div>
 							</div>
 							<input class="form-control input-lg" style="font-weight:bold"
 								type="text" value="" readonly>
 						</div>
 						<div class="modal-footer">
-							<form action="pass.apply.php" method="post">
+								<input type="hidden" name=hiddenID value='.$rows['matchID'].'>
 								<button type="button" class="btn btn-default" data-dismiss="modal">關閉</button>
-								<input type="submit" class="btn btn-primary" value="確認報名" onclick="Register(aa)">
-							</form>
+								<input type="submit" class="btn btn-primary" value="確認報名">
 						</div>
-					</div>
-					</div>
-				</div>';
+						</div>
+						</div>
+					</div></form>';
+					}
 				}
 				?>
 			</tbody>
 		</table>
-		<form action="pass.apply.php" method="post">
-			<Input type="button" class="btn btn-primary btn-lg" value="登出"
-				onclick="location.href='log.logout.php'">
-		</form>
+		<Input type="button" class="btn btn-primary btn-lg" 
+			value="登出" onclick="location.href='log.logout.php'">
 	</div>
 	<?php 
-	function Register($sql) {
-		echo "<script>alert('$sql')</script>";
-	}
-	if(isset($_POST['matID'])){
-		$fade_MatchID = $_POST['matID'];
+		//INSERT INTO `game` (`userID`, `matchID`) VALUES ('10', '11')
+	if (isset($_POST['hiddenID'])){
+		$uID =  preg_replace("/[\'\"]+/" , '' ,$_SESSION['uID']);
+		$mID =  preg_replace("/[\'\"]+/" , '' ,$_POST['hiddenID']);
+		$sqlApply = "INSERT INTO `game` (`userID`, `matchID`) VALUES ('$uID', '$mID')";
+		if(@mysql_query($sqlApply)){
+			echo "<script>alert('報名成功');window.location = 'pass.apply.php';</script>";
+		}else{
+			echo "<script>alert('報名失敗/);</script>";
+			echo $uID;
+		}
 	}
 	?>
-	<div class="modal fade" id="myModal" tabindex="-1" role="dialog"
-		aria-labelledby="myModalLabel">
-		<div class="modal-dialog" role="document">
-			<div class="modal-content">
-				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal"
-						aria-label="Close">
-						<span aria-hidden="true">&times;</span>
-					</button>
-					<h4 class="modal-title" id="myModalLabel">梯次<?php echo $fade_MatchID; ?></h4>
-				</div>
-				<div class="modal-body">
-					<p>自動排水舟</p>
-					<p>動力舟</p>
-					
-				</div>
-				<div class="modal-footer">
-					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-					<button type="button" class="btn btn-primary">Save changes</button>
-				</div>
-			</div>
-		</div>
-	</div>
 </body>
 </html>
